926ce978e38afe7c1a097cceb7fe402c8904f24b
Ryan Baumann <ryan.baumann@gmail.com>
2016-10-27 10:10:08 -0400

    Use pre/code for uploaded CSV fragment

--- geocollider-sinatra.rb
+++ geocollider-sinatra.rb
@@ -53,7 +53,7 @@ class GeocolliderSinatra < Sinatra::Base
       f.write(params['csvfile'][:tempfile].read)
     end
 
-    @csv_preview = File.foreach(@uploaded_filename).first(3).join('<br/>')
+    @csv_preview = File.foreach(@uploaded_filename).first(3).join("\n")
     haml :post_upload
   end
 
