a69ac6cb1aa62c3496054b222e4b16a4ac91db9a
Ryan Baumann <ryan.baumann@gmail.com>
2017-06-22 15:33:44 -0400

    Omit OpenRefine endpoint from rate limiting (fixes #31)

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -44,7 +44,7 @@ class Rack::Attack
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
   throttle('req/ip', :limit => 30, :period => 1.minutes) do |req|
-    req.ip # unless req.path.start_with?('/assets')
+    req.ip unless req.path.start_with?('/reconcile')
   end
 
   ### Prevent Brute-Force Login Attacks ###
